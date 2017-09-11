e4036197c35bcfa955deb5933af45f6a2989dbd4
LAPTOP-FSUTQ4RK\Q <qaqal2017@gmail.com>
2017-09-11 12:52:46 +0800

    deploy SHELL

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 # 代码更新
 git pull
 
+# 装依赖
+apt-get update
+apt-get install -y git python3 python3-pip zsh
+apt-get install -y nginx mongodb supervisor
+
+pip3 install -U pip setuptools wheel
+pip3 install jinja2 flask pymongo gunicorn
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # 删掉 nginx default 设置
 rm -f /etc/nginx/sites-enabled/default
 rm -f /etc/nginx/sites-available/default
@@ -11,9 +21,6 @@ ln -s -f /root/per-forum/per-forum.conf /etc/supervisor/conf.d/per-forum.conf
 # 不要再 sites-available 里面放任何东西
 ln -s -f /root/per-forum/per-forum.nginx /etc/nginx/sites-enabled/per-forum
 
-# 设置文件夹权限给nginx用
-chmod -R o+rx /
-chmod -R o+xr /root/per-forum
 
 # 重启服务器
 service supervisor restart
